0aeb0990f10c710eea247eab2053ce00661f34a8
Mallikharjuna Kiran Juvvala <Mallikharjuna.J@INDECOMM.LOCAL>
2017-08-02 17:57:35 +0530

    NEWFI-800 :CE Portal issue:Few ZIP codes showing Lender Fee as zero and few ZIP codes has values

--- js/handlebars-helpers.js
+++ js/handlebars-helpers.js
@@ -13,5 +13,11 @@ Handlebars.registerHelper({
   },
   loanRate: function (str) {
     return str.toFixed(3)+'%';
+  },
+  ifCond: function(v1, v2, options) {
+    if(v1 == v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
   }
 });
